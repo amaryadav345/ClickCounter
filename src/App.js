c6fdbf74456660a7cf033b0e6bf2f3e662b400b9
amaryadav345 <amaryadav345@gmail.com>
2021-08-03 17:45:36 +0530

    Added Material UI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import "./App.css";
 import React, { useState } from "react";
+import Button from '@material-ui/core/Button';
+import AddIcon from '@material-ui/icons/Add';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 const App = () => {
   const [count, setCount] = useState(0);
@@ -23,12 +26,14 @@ const App = () => {
       <div className="card">
         <h1>Counter App</h1>
         {count ? <h1>{count}</h1> : <h1 id="text-display">{count}</h1>}
-        <button className="incr" onClick={onAdd}>
-          Increament
-        </button>
-        <button className="decr" onClick={onSub}>
-          Decreament
-        </button>
+        <div className="buttonDiv">
+        <Button id="incr" onClick={onAdd}>
+          <AddIcon/>
+        </Button>
+        <Button id="decr" onClick={onSub}>
+          <DeleteIcon/>
+        </Button>
+        </div>
       </div>
     </div>
   );
